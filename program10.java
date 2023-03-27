d1b21beced4a34b6f3a6df3fe47ffe6ac6833e0a
upparauppara <129029896+upparauppara@users.noreply.github.com>
2023-03-27 10:46:52 +0530

    Add files via upload

---
+++ program10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class program10
+{
+public static void main(String args[])
+{
+int counter;
+counter=1;
+while(counter<=10)
+{
+System.out.println("welcome !");
+counter=counter+1;
+}
+}
+}
