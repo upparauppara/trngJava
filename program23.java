3b58f00134c008e1a0cd1da74795e852499a185c
upparauppara <129029896+upparauppara@users.noreply.github.com>
2023-03-27 10:37:47 +0530

    Add files via upload

---
+++ program23.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class program23
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+switch(num)
+{
+case 1:{System.out.print("JAN"); break;}
+case 2:{System.out.print("FEB");break;}
+case 3:{System.out.print("MARCH"); break;} 
+case 4:{System.out.print("APRIL");break;}
+case 5:{System.out.print("MAY"); break;}
+case 6:{System.out.print("JUNE");break;}
+case 7:{System.out.print("JULY"); break;}
+case 8:{System.out.print("AUG");break;}
+case 9:{System.out.print("SEP"); break;} 
+case 10:{System.out.print("OCT");break;}
+case 11:{System.out.print("NOV"); break;}
+case 12:{System.out.print("DEC");break;}
+
+}
+}
+}
+
+
+
+
+
---
+++ program25.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class program25
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int num1=sc.nextInt();
+int num1=sc.nextInt();
+System.out.print/n("1:Addition");
+System.out.print/n("2:Substraction");
+System.out.print/n("3:Multiplication");
+System.out.print/n("4:Divionion");
+System.out.print/n("5:Modulus Division");
+int choice=sc.nextInt();
+if (choice==1)System.out.println(num1+"+"+num2+"="+(num1+num2));
+else if (choice==2)System.out.println(num1+"-"+=num2+"="+(num1-num2));
+else if (choice==3)System.out.println(num1+"*"+num2+"="+(num1*num2));
+else if (choice==4)System.out.println(num1+"/"+num2+"="+(num1/num2));
+else if (choice==5)System.out.println(num1+"%"+num2+"="+(num1%num2));
+}
+}
+
+
+
+
+
+
+
+
