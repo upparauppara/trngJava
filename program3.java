c65f57ef2125ba4548dd7f12f05416b0e6d21eed
upparauppara <129029896+upparauppara@users.noreply.github.com>
2023-03-27 10:45:45 +0530

    Add files via upload

---
+++ program3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class program3
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int num1=sc.nextInt();
+int num2=sc.nextInt();
+int num3=sc.nextInt();
+int num4=sc.nextInt();
+{
+   System.out.print(num1+"/"+num2+"*"+num3+"*"+num4+"="+(num1/num2*num3*num4));
+  
+}
+}
+}
