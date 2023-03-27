be5a45327cfee54da5c83b033d72ce4e77a7294f
upparauppara <129029896+upparauppara@users.noreply.github.com>
2023-03-27 10:34:48 +0530

    Add files via upload

---
+++ Anithaself.java
@@ -0,0 +1,9 @@
+class Anithaself
+{
+public static void main(String args[]){
+System.out.print("Anitha");
+System.out.print("I am from 3rd btech");
+System.out.print("In AM REDDYcollage");
+System.out.print("ECE branch");
+}//main()
+}//Anithaself
