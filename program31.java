1c40fc35cc5fc593201f2c5c5c14e34174076beb
upparauppara <129029896+upparauppara@users.noreply.github.com>
2023-03-27 10:34:24 +0530

    Add files via upload

---
+++ program31.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class program31
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int num1=sc.nextInt();
+int num2=sc.nextInt();
+switch(Num )
+{
+{System.out.println("1:Addition");break;}
+{System.out.println("2:Substraction");break;}
+{System.out.println("3:Multiplication");break;}
+{System.out.println("4:Divionion");break;}
+{System.out.println("5:Modulus Division");break;}
+int choice=sc.nextInt();
+case 1:(choice==1)System.out.println(num1+"+"+num2+"="+(num1+num2));
+case 2:(choice==2)System.out.println(num1+"-"+num2+"="+(num1-num2));
+case 3:(choice==3)System.out.println(num1+"*"+num2+"="+(num1*num2)); 
+case 4:(choice==4)System.out.println(num1+"/"+num2+"="+(num1/num2));
+case 5:(choice==5)System.out.println(num1+"%"+num2+"="+(num1%num2));
+}
+}
+
+}
+
+
+
+
+
+
