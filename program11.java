ef02b89f7e4d3892228010262edef8cdd4d61537
upparauppara <129029896+upparauppara@users.noreply.github.com>
2023-03-27 10:47:44 +0530

    Add files via upload

---
+++ program11.java
@@ -0,0 +1,12 @@
+Class program11
+{
+Public static void main(string args[]);
+{
+int num1=5;
+int num2=7;
+int res=num1+num2;
+system.out.print(num1+"+"+num2+"="+res);
+int res=num1-num2;
+System.out.print(num1+"-"num2+"="+res);
+}//main()
+}//program11
---
+++ program12.java
@@ -0,0 +1,12 @@
+Class program12
+{
+Public static void main(String args[]);
+{
+int num1=5;
+int num2=7;
+int res=num1+num2;
+System.out.print(num1+ "+" +num2+ "=" +res);
+int res=num1-num2;
+System.out.print(num1+ "-" num2+ "=" +res);
+}//main()
+}//Program12
